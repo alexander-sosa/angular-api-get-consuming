c4152e1527d5a7c2b24b758a39108ea8f644b70d
alexander-sosa <junior.sosa@ucb.edu.bo>
2022-01-16 21:19:26 -0400

    demo finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,16 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+// estos ayudan a lidiar con los apis
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+import { Meme } from './meme';
+
+/*
+esta clase contiene la info general a correr del programa. Se llama app-root y su contenido se divide en
+app.component.html y app.component.css
+*/
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +18,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'meme-project';
+  public mms : Meme[] = [];
+
+  constructor(private http: HttpClient){
+
+  }
+
+  onSave() : void{
+    console.log("Antes de invocar el REST");
+    this.http.get<any>("https://api.imgflip.com/get_memes") // esto devuelve un Observable
+      .subscribe(response => { //al llegar los datos asincronamente, los vamos a procesar aqui
+        console.log(response, "Llego los datos");
+        this.mms = response.data.memes;
+      });
+    console.log("Luego de invocar el REST");
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// aqui y en imports se agrega el HttpClientModule para poder trabar con el
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
